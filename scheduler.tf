52629655537779710fa95cef9e2582eaf24e9795
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-24 21:25:05 +0300

    Add the missing permissions

--- scheduler.tf
+++ scheduler.tf
@@ -30,3 +30,11 @@ resource "aws_cloudwatch_event_target" "lambda_timely_invoked_target" {
     }
     EOF
 }
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_timely_invoked_function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_timely_invoked_schedule.arn
+}
