273df05d8cb2404804b715de6275b0546eaa06d2
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-24 21:30:30 +0300

    Remove pattern and lambda structure

--- scheduler.tf
+++ scheduler.tf
@@ -2,22 +2,6 @@ resource "aws_cloudwatch_event_rule" "lambda_timely_invoked_schedule" {
   name                = "lambda-timely-invoked-schedule"
   description         = "Schedule to trigger the lambda function"
   schedule_expression = "rate(5 minutes)"
-
-  event_pattern = <<PATTERN
-    {
-        "source": [
-            "aws.config"
-        ],
-        "detail-type": [
-            "Config Rules Compliance Change"
-        ],
-        "detail": {
-            "messageType": [
-                "ComplianceChangeNotification"
-            ]
-        }
-    }
-    PATTERN
 }
 
 resource "aws_cloudwatch_event_target" "lambda_timely_invoked_target" {
