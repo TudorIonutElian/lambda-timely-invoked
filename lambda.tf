b94c192d4779687259d6295a33c9c0a548411d65
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-24 21:14:31 +0300

    Fix role ARN typo

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda_timely_invoked_function" {
   filename         = "lambda_function_payload.zip"
   function_name    = "lambda-timely-invoked-func"
-  role             = aws_iam_role.lambda_timely_invoked_iam_role
+  role             = aws_iam_role.lambda_timely_invoked_iam_role.arn
   handler          = "index.handler"
   runtime          = "nodejs20.x"
   source_code_hash = data.archive_file.lambda_timely_invoked_function_archive.output_base64sha256
