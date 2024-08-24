c8481e2a88d3a2a59dbdb9f2147035b0b7240906
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-24 21:08:38 +0300

    Change the lambda zip

--- lambda.tf
+++ lambda.tf
@@ -3,6 +3,6 @@ resource "aws_lambda_function" "lambda_timely_invoked_function" {
   function_name    = "lambda-timely-invoked-func"
   role             = aws_iam_role.lambda_timely_invoked_iam_role
   handler          = "index.handler"
-  runtime          = "nodejs12.x"
-  source_code_hash = aws_data_archive_file.lambda_timely_invoked_archive.output_base64sha256
+  runtime          = "nodejs20.x"
+  source_code_hash = filebase64sha256("lambda.zip")
 }
--- lambda/index.js
+++
@@ -1,9 +0,0 @@
-
-const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda Timely Invoked!'),
-  };
-};
-
-export { handler };
