5f67fab55133d551c140bd5fce96f33ff5fcceca
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-24 21:17:32 +0300

    Add some messages to the lambda

--- scheduler.tf
+++ scheduler.tf
@@ -2,9 +2,31 @@ resource "aws_cloudwatch_event_rule" "lambda_timely_invoked_schedule" {
   name                = "lambda-timely-invoked-schedule"
   description         = "Schedule to trigger the lambda function"
   schedule_expression = "rate(5 minuts)"
+
+  event_pattern = <<PATTERN
+    {
+        "source": [
+            "aws.config"
+        ],
+        "detail-type": [
+            "Config Rules Compliance Change"
+        ],
+        "detail": {
+            "messageType": [
+                "ComplianceChangeNotification"
+            ]
+        }
+    }
+    PATTERN
 }
 
 resource "aws_cloudwatch_event_target" "lambda_timely_invoked_target" {
   rule = aws_cloudwatch_event_rule.lambda_timely_invoked_schedule.name
   arn  = aws_lambda_function.lambda_timely_invoked_function.arn
+
+  input = <<EOF
+    {
+        "MESSAGE": "Hello from CloudWatch Events!"
+    }
+    EOF
 }
